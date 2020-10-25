17076bb49c664296b5c080282b72733dea917fad
Debian <bitnami@ip-172-26-9-43.eu-central-1.compute.internal>
2020-10-25 17:36:35 +0000

    changes

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,101 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
+{{--@section('content')--}}
+{{--<div class="container">--}}
+{{--    <div class="row justify-content-center">--}}
+{{--        <div class="col-md-8">--}}
+{{--            <div class="card">--}}
+{{--                <div class="card-header">{{ __('Verify Your Email Address') }}</div>--}}
+
+{{--                <div class="card-body">--}}
+{{--                    @if (session('resent'))--}}
+{{--                        <div class="alert alert-success" role="alert">--}}
+{{--                            {{ __('A fresh verification link has been sent to your email address.') }}--}}
+{{--                        </div>--}}
+{{--                    @endif--}}
+
+{{--                    {{ __('Before proceeding, please check your email for a verification link.') }}--}}
+{{--                    {{ __('If you did not receive the email') }},--}}
+<form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
+    @csrf
+    <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>
+    .
+</form>
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--@endsection--}}
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+    <section class="ftco-section bg-light">
+        <div class="container">
+            <div class="row justify-content-center ">
+                <div class="col-md-12">
+                    <div class="wrapper">
+                        <div class="row no-gutters ">
+                            <div class="col-lg-12 col-md-7 order-md-last d-flex align-items-stretch">
+                                <div class="contact-wrap w-100 p-md-5 p-4 ">
+                                    <h3 style=" text-align: center;  color: white;font-weight: 900; font-size: 50px;">
+                                        Verify Your Email
+                                    </h3>
+                                    <h4>
+                                        @if (session('resent'))
+                                            <div class="alert alert-success" role="alert">
+                                                {{ __('A fresh verification link has been sent to your email address.') }}
+                                            </div>
+                                        @endif
+                                    </h4>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                                    <h3 class="mb-4" style=" text-align: center;color: #b6b6b6;font-weight: 600;">
+                                        {{ __('Before proceeding, please check your email for a verification link.') }}
+                                        {{ __('If you did not receive the email') }},
+                                        <form class="d-inline" method="POST"
+                                              action="{{ route('verification.resend') }}">
+                                            @csrf
+                                            <button type="submit"
+                                                    style="font-weight: 600;font-size: 25px"
+                                                    class="text-white btn btn-link align-baseline">
+                                                <u>{{ __('click here to request another') }}</u>
+                                            </button>
+                                            .
+                                        </form>
+                                    </h3>
+                                    <br>
+                                </div>
+                            </div>
+
+
                         </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
 @endsection
+
+@section('footer')
+    <footer class="footer mt-4">
+        <div class="container-fluid px-lg-5">
+            <div class="row">
+                <div class="col-md-9 py-5">
+                    <div class="row">
+                        <div class="col-md-4 mb-md-0 mb-4">
+                            <h2 class="footer-heading">Powered by tiplify - das bargelslose Trinkgeld </h2>
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-md-3 py-md-5 py-4 aside-stretch-right pl-lg-5">
+                    <h2 class="footer-heading">AGB | Datenschutz | Impressum</h2>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+
+@endsection
+
