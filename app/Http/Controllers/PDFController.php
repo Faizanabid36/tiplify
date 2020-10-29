2ca3b626217bd5b33a6a9be8aefa5f467401a493
Faizan Abid <Faizanabid36@gmail.com>
2020-10-29 14:13:39 +0500

    Finalization

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -19,6 +19,7 @@ class PDFController extends Controller
         $img2 = public_path('images/2.png');
         $img3 = public_path('images/3.png');
         $logo = public_path('assets/images/logo2.png');
+        dd($logo);
         $img4 = public_path('/qrcodes/' . $user->restaurant->unique_key . '.svg');
         $link = route('corona_form.view', $user->restaurant->unique_key);
         $firmname = $user->restaurant->firmname;
